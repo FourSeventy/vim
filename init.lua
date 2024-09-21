592facea9a4520998c623aa5f0c93348bfd1c1f9
Mike Signorella <msiggy@gmail.com>
2024-09-21 15:04:27 -0400

    Set theme

--- init.lua
+++ init.lua
@@ -669,20 +669,19 @@ require('lazy').setup({
     },
   },
 
-
-
+  -- installing some colorschemes
+  { 'folke/tokyonight.nvim' },
+  { "ellisonleao/gruvbox.nvim" },
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    -- 'rebelot/kanagawa.nvim',
-    'folke/tokyonight.nvim',
+    'rebelot/kanagawa.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
-      -- vim.cmd.colorscheme 'kanagawa-dragon'
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'kanagawa'
 
       -- You can configure highlights by doing something like:
       -- vim.cmd.hi 'Comment gui=none'
